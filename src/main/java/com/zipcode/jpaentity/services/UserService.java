ff8a1179633945991bda26c621a7aa4c66ee99a4
Daniel Moffett <moffd234@gmail.com>
2026-03-16 02:13:30 -0400

    Wrote findAll

--- src/main/java/com/zipcode/jpaentity/services/UserService.java
+++ src/main/java/com/zipcode/jpaentity/services/UserService.java
@@ -2,6 +2,9 @@ package com.zipcode.jpaentity.services;
 
 import com.zipcode.jpaentity.entities.User;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class UserService {
     private final EntityManager entityManager;
@@ -10,8 +13,15 @@ public class UserService {
         this.entityManager = entityManager;
     }
 
-    public User findById(int id){
+    public User findById(int id) {
         return entityManager.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        String query = "SELECT u FROM User u"; // Select <alias> from <entityClass (not table name) <alias>
+        TypedQuery<User> typedQuery = entityManager.createQuery(query, User.class);
+
+        return typedQuery.getResultList();
     }
 
 
